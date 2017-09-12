0f871effe6406a1ec4f24aafe29d17b773784c3a
Tim Joosten <Tjoosten3@gmail.com>
2017-09-13 01:32:27 +0200

    Implementatie missende test factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -32,3 +32,25 @@ $factory->define(ActivismeBE\Role::class, function (Faker\Generator $faker) {
 $factory->define(ActivismeBE\Permission::class, function (Faker\Generator $faker) {
     return ['name' => $faker->word];
 });
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(ActivismeBE\Contact::class, function (Faker\Generator $faker) {
+    return [
+        'read_by' => function () {
+            return factory(ActivismeBE\User::class)->id;
+        },
+        'is_read' => 'N',
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'email' => $faker->email,
+        'subject' => $faker->sentence,
+        'message' => $faker->sentences(3)
+    ];
+});
+
+/** @var \Illuminate\Database\Eloquent\Factory $faker */
+$factory->define(ActivismeBE\SingleSupport::class, function (Faker\Generator $faker) {
+   return [
+
+   ];
+});
